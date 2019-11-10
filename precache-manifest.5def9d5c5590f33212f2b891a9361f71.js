8c3d7d2e7dfa12b56b8e08d317c47d188bb356df
shiho ishitoko <cidermitaina@gmail.com>
2019-11-10 21:04:50 +0900

    Updates

---
+++ precache-manifest.5def9d5c5590f33212f2b891a9361f71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f1492a42b610288e41d01c61c7147fc",
+    "url": "/tea/index.html"
+  },
+  {
+    "revision": "81c117a17bb58e5f8bd1",
+    "url": "/tea/static/js/2.ad341df5.chunk.js"
+  },
+  {
+    "revision": "27e2da150e54756007e2",
+    "url": "/tea/static/js/main.05751380.chunk.js"
+  },
+  {
+    "revision": "ec385e8679e0b7f4ec2d",
+    "url": "/tea/static/js/runtime-main.2751e3a4.js"
+  },
+  {
+    "revision": "cc5e75863ffd24ac8b985d5025a4f236",
+    "url": "/tea/static/media/ReadingSideDoodle.cc5e7586.svg"
+  }
+]);
